52069aa3ca53da59d65d42e544cd0dbb508a8923
zbeyens <kindaichilogins@gmail.com>
2018-05-29 01:53:21 +0200

    fill occlusion gpu

--- stereo_matching_cuda/occlusion.cu
+++ stereo_matching_cuda/occlusion.cu
@@ -19,7 +19,7 @@ void detect_occlusion(float* disparityLeft, float* disparityRight, const float d
 
 	int n = w * h;
 
-	memset(disparityLeft, 0, n * sizeof(float));
+	//memset(disparityLeft, 0, n * sizeof(float));
 	CHECK(cudaMalloc((void**)&d_disparityLeft, n * sizeof(float)));
 	CHECK(cudaMalloc((void**)&d_disparityRight, n * sizeof(float)));
 
@@ -60,33 +60,83 @@ void detect_occlusionOnCPU(float* disparityLeft, float* disparityRight, const fl
 
 void fill_occlusion(float* disparity, const int w, const int h, const float vMin)
 {
-	//float* d_disparity;
+	float* d_disparity;
 
-	//int n = w * h;
+	int n = w * h;
 
-	//memset(disparity, 0, n * sizeof(float));
-	//CHECK(cudaMalloc((void**)&d_disparity, n * sizeof(float)));
-	//CHECK(cudaMalloc((void**)&d_disparityRight, n * sizeof(float)));
+	CHECK(cudaMalloc((void**)&d_disparity, n * sizeof(float)));
 
-	//CHECK(cudaMemcpy(d_disparity, disparity, n * sizeof(float), cudaMemcpyHostToDevice));
-	//CHECK(cudaMemcpy(d_disparityRight, disparityRight, n * sizeof(float), cudaMemcpyHostToDevice));
+	CHECK(cudaMemcpy(d_disparity, disparity, n * sizeof(float), cudaMemcpyHostToDevice));
 
-	//dim3 nThreadsPerBlock(1024);
-	//dim3 nBlocks((n + nThreadsPerBlock.x - 1) / nThreadsPerBlock.x);
+	dim3 nThreadsPerBlock(1024);
+	dim3 nBlocks((n + nThreadsPerBlock.x - 1) / nThreadsPerBlock.x);
 
-	//detect_occlusionOnGPU << <nBlocks, nThreadsPerBlock >> > (d_disparity, d_disparityRight, dOcclusion, dLR, w, h);
+	fill_occlusionOnGPU1 << <nBlocks, nThreadsPerBlock >> > (d_disparity, w, h, vMin);
 
-	//CHECK(cudaDeviceSynchronize());
-	//CHECK(cudaGetLastError());
+	CHECK(cudaDeviceSynchronize());
+	CHECK(cudaGetLastError());
 
-	//CHECK(cudaMemcpy(disparity, d_disparity, n * sizeof(float), cudaMemcpyDeviceToHost));
+	CHECK(cudaMemcpy(disparity, d_disparity, n * sizeof(float), cudaMemcpyDeviceToHost));
 
-	//CHECK(cudaFree(d_disparity));
+	CHECK(cudaFree(d_disparity));
+}
+
+__global__ void fill_occlusionOnGPU1(float* disparity, const int w, const int h, const float vMin)
+{
+	int tdx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (tdx >= w * h) return;
+
+	int dX = disparity[tdx];
+	// ignore non-occluded pixels
+	if (dX >= vMin) return;
+
+	int y = tdx / w;
+
+	int xLeft = tdx % w;
+	float dLeft = vMin;
+
+	// find first non occluded left pixel
+	while (xLeft >= 0)
+	{
+		if (disparity[xLeft + w * y] >= vMin) {
+			dLeft = disparity[xLeft + w * y];
+			break;
+		}
+
+		xLeft -= 1;
+	}
+
+	int xRight = tdx % w;
+	float dRight = vMin;
+
+	// find first non occluded right pixel
+	while (xRight < w)
+	{
+		// if it is nonoccluded, stop
+		if (disparity[xRight + w * y] >= vMin) {
+			dRight = disparity[xRight + w * y];
+			break;
+		}
+
+		xRight += 1;
+	}
+
+	disparity[tdx] = max(dLeft, dRight);
+}
+
+
+__global__ void fill_occlusionOnGPU2(float* disparity, const int w, const int h, const float vMin)
+{
+	// 1) threads computing dLeft
+	// 2) threads computing dRight
+	// syncthreads
+	// max(dLeft, dRight)
 }
 
 /// Fill pixels below value vMin using maximum value between the two closest (at left and at right) pixels on same
 /// line above vMin.
-void fillXOnCPU(float* disparity, const int w, const int h, const float vMin, const bool isMin) {
+void fill_occlusionOnCPU(float* disparity, const int w, const int h, const float vMin) {
 	for (int y = 0; y < h; y++) {
 		for (int x = 0; x < w; x++)
 		{
